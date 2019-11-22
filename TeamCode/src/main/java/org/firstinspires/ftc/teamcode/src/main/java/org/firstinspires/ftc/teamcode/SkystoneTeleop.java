bd3701eabe42b65483364cdc03c5ac0abae8b123
suarezm <suarezm@greenhill.org>
2019-11-22 17:07:39 -0600

    reverse teleop turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneTeleop.java
@@ -89,7 +89,7 @@ public class SkystoneTeleop extends OpMode {
         double drive = .8f;
         double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
         double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
-        double rightX = gamepad1.right_stick_x;
+        double rightX = -gamepad1.right_stick_x;
         final double v1 = r * Math.cos(robotAngle) + rightX;
         final double v2 = r * Math.sin(robotAngle) - rightX;
         final double v3 = r * Math.sin(robotAngle) + rightX;
